062a247f648741747665cabcd755347356a8f43f
Gaurav Gupta <44429156+gupta2022@users.noreply.github.com>
2020-09-30 19:40:44 +0530

    Update readAlert.py

--- readAlert.py
+++ readAlert.py
@@ -1,30 +1,36 @@
+import glob
 import extract_msg
 import re
 import requests
 from bs4 import BeautifulSoup
 
-f = r'Google Alert - Bribery India (61).msg'  # Replace with yours
-msg = extract_msg.Message(f)
-msg_sender = msg.sender
-msg_date = msg.date
-msg_subj = msg.subject
-msg_message = msg.body
+
+files=glob.glob("C:/Users/venig/Desktop/ML Stuff/ML Stuff/usefull/*.msg")
+for fl in files:
+    print (fl)
+    #f = r'Google Alert - Bribery India (61).msg'  # Replace with yours
+    f= r'%s' % fl
+    msg = extract_msg.Message(f)
+    msg_sender = msg.sender
+    msg_date = msg.date
+    msg_subj = msg.subject
+    msg_message = msg.body
 
 
 
-#print('Body: {}'.format(msg_message))
-#for line in msg_message:
-#print(msg_message)
-urls = re.findall('(?<=\<)(.*?)(?=\>)', msg_message)
-for url in urls:
-    #print(url)
-    z= re.search('source=alertsmail', url)
-    if z ==None :
-        furl=re.findall('=(?<=url=)(.*?)&ct', url)
-        if furl:
-            print(furl[0])
-            html_text = requests.get(furl[0]).text
-            soup = BeautifulSoup(html_text, 'html.parser')
-            print(soup.get_text())
-            print()
-            print()
+    #print('Body: {}'.format(msg_message))
+    #for line in msg_message:
+    #print(msg_message)
+    urls = re.findall('(?<=\<)(.*?)(?=\>)', msg_message)
+    for url in urls:
+        #print(url)
+        z= re.search('source=alertsmail', url)
+        if z ==None :
+            furl=re.findall('=(?<=url=)(.*?)&ct', url)
+            if furl:
+                print(furl[0])
+                html_text = requests.get(furl[0]).text
+                soup = BeautifulSoup(html_text, 'html.parser')
+                print(soup.get_text())
+                print()
+                print()
