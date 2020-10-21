5065653e54f089090d383c512893d33eace2657a
gupta2022 <44429156+gupta2022@users.noreply.github.com>
2020-10-21 15:16:11 +0530

    error in requests.urls

--- fetchurls.py
+++ fetchurls.py
@@ -4,8 +4,16 @@ import extract_msg
 import re
 import requests
 from bs4 import BeautifulSoup
-urls=[]
-list1=[]
+import pandas
+
+colnames = ['urls', 'tag']
+data = pandas.read_csv('ML dataset - Sheet1.csv', names=colnames)
+
+urls=data.urls.tolist()
+list1=data.tag.tolist()
+#print(urls)
+#print(list1)
+
 try:
     with open("articles.txt", "rb") as fp:   # Unpickling
         list_articles = pickle.load(fp)
@@ -13,13 +21,15 @@ try:
         list_article_label = pickle.load(fp)
 except :
     print("No file detected")
-for x,y in zip(urls,list1):
-  html_text = requests.get(x).text
-  soup = BeautifulSoup(html_text, 'html.parser')
-  #print(soup.get_text())
-  print([label,soup.get_text()])
-  list_articles.append(soup.get_text())
-  list_article_label.append(y)
+for (x,y) in zip(urls,list1):
+#  print(x)
+    html_text = requests.get(x).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    #print(soup.get_text())
+    print([label,soup.get_text()])
+    list_articles.append(soup.get_text())
+    list_article_label.append(y)
+
 with open("articles.txt", "wb") as fp:   #Pickling
   pickle.dump(list_articles, fp)
 
