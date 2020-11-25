a0d5b8cbcef4220e0b999dbadc59894c4ccbe5f9
gupta2022 <44429156+gupta2022@users.noreply.github.com>
2020-11-25 16:19:06 +0530

    Final script

--- cleanDataset.py
+++ cleanDataset.py
@@ -3,6 +3,7 @@ import pandas as pd
 import re
 
 def clean():
+    #ErrorList contains the list of expected errors for the scrapper
     errorList=["page not found","error 40","error 50", "are you a robot","the connection was interrupted",
     "the connection was reset","the connection has timed out","warning: potential security risk ahead",
     "secure connection failed","did not connect: potential security issue","did not connect",
@@ -13,26 +14,27 @@ def clean():
     except:
         print("Error")
 
-
+    #hasError checks if an error has occured and article was not feteched 
     def hasError(x):
         for error in errorList:
             if error in x:
                 return True
 
 
-    #print(data["article"].isnull().sum())
+    
     data=data.dropna()
     print(data)
-    #df.drop(index, inplace = True)
+    
     count=0
+    #iterates the articles to find if an error had occurred 
     for index,row in data.iterrows():
         row = row.copy()
         x=re.sub(r'\n\s*\n', '\n',row['article'] ).lower()
-        #x=row['article'].replace('\n\n','\n').lower()
+        
         if hasError(x):
             data.drop(index, inplace = True)
-            #print(x)
+            
             count+=1
-    print(data)
+    #print(data)
     data.to_pickle('cleanDataset.pkl')
-    print(count)
+    #print(count)
